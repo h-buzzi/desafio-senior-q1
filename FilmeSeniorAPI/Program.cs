20c445588be8fa5292dff326c61bb2ff2d68dca6
h-buzzi <h-buzzi>
2022-07-08 22:09:05 -0300

    Removido uso de CORS

--- FilmeSeniorAPI/Program.cs
+++ FilmeSeniorAPI/Program.cs
@@ -1,16 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        builder =>
-        {
-            builder.WithOrigins("https://localhost:7131","http://localhost:5131")
-                                .AllowAnyHeader()
-                                .AllowAnyMethod();
-        });
-});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -25,10 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 app.UseRouting();
-app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
